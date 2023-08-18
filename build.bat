9672dab8029b9653caaeee70f75244fb1ab54f16
katatunix <katatunix@gmail.com>
2023-08-18 23:12:37 +0700

    correct copy cmd

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 msbuild -t:Build -p:Configuration=Release -m:2
 
 md build
-copy sweet/bin/Release/sweet.exe build
-copy sweet/bin/Release/*.dll build
-copy sweet/sweet.cfg build
-copy stop/bin/Release/stop.exe build
+copy sweet\bin\Release\sweet.exe build
+copy sweet\bin\Release\*.dll build
+copy sweet\sweet.cfg build
+copy stop\bin\Release\stop.exe build
